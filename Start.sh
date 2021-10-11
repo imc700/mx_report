2631a86f467768d669b3be964f2efbd20b5668af
mac_imc700 <mac_imc700@163.com>
2021-10-11 19:34:15 +0800

    q_tx for vue2

--- Start.sh
+++ Start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #这里使用的前提是通过ssh设置免密登录3
 scp ./Deploy.sh root@110.42.210.34:/hx/vue_tools.sh
-ssh -tt root@116.63.138.138 << eeooff
+ssh -tt root@110.42.210.34 << eeooff
 sh /hx/vue_tools.sh
 exit
 eeooff
